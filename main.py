7baaed86a0685dce3e20ad59280386f7430ee486
Cyberis <christopher.bortz@gmail.com>
2025-12-06 21:40:33 -0500

    Print Nice Report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Bookbot -- main.py - Analyize text versions of books and provide information (entry point).
-from stats import book_word_count, character_counts
+from stats import book_word_count, character_counts, sorted_character_counts    
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -7,12 +7,22 @@ def get_book_text(file_path):
 
     
 def main():
+    # Get book properties
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = book_word_count(book_text)
-    print(f"Found {word_count} total words")
     char_counts = character_counts(book_text)
-    print(char_counts)
+    sorted_char_counts = sorted_character_counts(char_counts)
+    
+    # Print out report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_count in sorted_char_counts:
+        print(f'{char_count["char"]}: {char_count["num"]}')
+    print("============= END ===============")
     
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,5 +1,8 @@
 # Bookbot -- stats.py - Analyze text versions of books and provide statistical information.
 
+def sort_on(item):
+    return item["num"]
+
 def book_word_count(book_text):
     words = book_text.split()
     return len(words)
@@ -15,3 +18,12 @@ def character_counts(book_text):
             char_counts[char] = 1
             
     return char_counts
+
+def sorted_character_counts(char_counts):
+    count_list = []
+    for char, count in char_counts.items():
+        if not char.isalpha():
+            continue
+        count_list.append({"char": char, "num": count})
+        count_list.sort(key=sort_on, reverse=True)
+    return count_list
